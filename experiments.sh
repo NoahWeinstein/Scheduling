c938473bae4e5cc1ee7bdf405d154f7151557586
Noah Weinstein <nweinstein@haverford.edu>
2016-11-13 19:59:33 -0500

    working on graphs

--- experiments.sh
+++ experiments.sh
@@ -109,6 +109,7 @@ do
 done
 echo '\n'
 
+# This is varying students again
 echo 'Experiment 7 has 10 time slots, 12 rooms,  100 classes, and 8000
 students \n'
 echo 'Experiment 3' >> values.txt
@@ -126,6 +127,41 @@ do
     rm schedule_1.txt
 done
 echo '\n'
+
+echo 'Experiment 8 has 10 time slots, 12 rooms,  100 classes, and 16000
+students \n'
+echo 'Experiment 8' >> values.txt
+#echo 'Experiment 1' >> times.txt
+
+for i in `seq 1 $max`
+do
+    ./make_random_input.pl 12 100 10 16000 constraints_1.txt prefs_1.txt
+    time python make_schedule.py constraints_1.txt prefs_1.txt schedule_1.txt
+    ./exp_is_valid.pl constraints_1.txt prefs_1.txt schedule_1.txt >> values.txt
+    echo '\t 64000' >> values.txt
+
+    rm constraints_1.txt
+    rm prefs_1.txt
+    rm schedule_1.txt
+done
+echo '\n'
+
+# and this is classes
+echo 'Experiment 9 has 10 time slots, 192 rooms,  1600 classes, and 1000 students'
+echo 'Experiment 9' >> values.txt
+for i in `seq 1 $max`
+do
+    ./make_random_input.pl 192 1600 10 1000 constraints_4.txt prefs_4.txt
+    time python make_schedule.py constraints_4.txt prefs_4.txt schedule_4.txt
+    ./exp_is_valid.pl constraints_4.txt prefs_4.txt schedule_4.txt >> values.txt
+    echo '\t 4000' >> values.txt
+
+    rm constraints_4.txt
+    rm prefs_4.txt
+    rm schedule_4.txt
+done
+echo '\n'
+
 
 
 
